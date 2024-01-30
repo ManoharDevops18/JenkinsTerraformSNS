70b69aa74342baf60036588eb17a0c9b4ea94c8c
ManoharDevops18 <132356003+ManoharDevops18@users.noreply.github.com>
2024-01-30 22:52:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIAWM43CGW6WBBXELME"
+  secret_key = "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"
+}
 resource "aws_sns_topic" "user_updates" {
   name = "user-updates-topic"
 }
